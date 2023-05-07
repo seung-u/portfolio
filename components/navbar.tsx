b57bdce1ac9807944379ba06337235ed0dd0e536
백승우 <>
2023-05-07 11:57:18 +0900

    image optimized💯

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+import Image from "next/image";
+import logo from "../public/logo.png";
+import hamburger from "../public/hamburger.png";
 
 function tgltab() {
   const elmt = document.getElementById("navbar-default");
@@ -6,72 +8,74 @@ function tgltab() {
 }
 
 const Navbar = () => (
-  <div className="bg-white border-gray-200 dark:bg-gray-900">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="./" className="flex items-center">
-        <img src="logo.png" className="h-8 mr-3" alt="logo" />
-        <span
-          id="logo-font"
-          className="self-center text-2xl font-mono font-bold whitespace-nowrap dark:text-white"
+  <>
+    <div className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="./" className="flex items-center">
+          <Image id="logo" src={logo} width={32} alt="LOGO" />
+          <span
+            id="logo-font"
+            className="ml-3 self-center text-2xl font-mono font-bold whitespace-nowrap dark:text-white"
+          >
+            @seung-u
+          </span>
+        </a>
+        <button
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          onClick={tgltab}
         >
-          @seung-u
-        </span>
-      </a>
-      <button
-        className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        onClick={tgltab}
-      >
-        <span className="sr-only">Open Main Menu</span>
-        <img src="hamburger.png" alt="More" className="w-6 h-6"></img>
-      </button>
+          <span className="sr-only">Open Main Menu</span>
+          <Image src={hamburger} width={24} alt="MORE" />
+        </button>
 
-      <div id="navbar-default" className="hidden w-full md:block md:w-auto">
-        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <li>
-            <a
-              href="./"
-              className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-              aria-current="page"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="/projects"
-              className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
-              Projects
-            </a>
-          </li>
-          <li>
-            <a
-              href="/notion"
-              className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
-              Notion
-            </a>
-          </li>
-          <li>
-            <a
-              href="/github"
-              className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
-              Github
-            </a>
-          </li>
-          <li>
-            <a
-              href="/about"
-              className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
-              About
-            </a>
-          </li>
-        </ul>
+        <div id="navbar-default" className="hidden w-full md:block md:w-auto">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="./"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="/projects"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                href="/notion"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Notion
+              </a>
+            </li>
+            <li>
+              <a
+                href="/github"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                href="/about"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
+  </>
 );
 
 export default Navbar;
