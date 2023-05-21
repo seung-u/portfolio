53ae75f0da44d3f79546c163bf4ce642b3e2a0d1
seung-u <xjinseo@gmail.com>
2023-05-21 13:04:09 +0900

    linked notion page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,6 @@ function tgltab() {
   const elmt = document.getElementById("navbar-default");
   elmt?.classList.toggle("hidden");
 }
-
 const Navbar = () => (
   <>
     <div className="bg-white border-gray-200 dark:bg-gray-900">
@@ -53,7 +52,7 @@ const Navbar = () => (
             </li>
             <li>
               <a
-                href="/notion"
+                href="www.notion.seung-u.com"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Notion
@@ -81,5 +80,4 @@ const Navbar = () => (
     </div>
   </>
 );
-
 export default Navbar;
